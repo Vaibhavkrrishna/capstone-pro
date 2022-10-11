eb2c8756d9737bac836d39f5daff60eac5ebe639
Vaibhavkrrishna <kvaibhavloyola@gmail.com>
2022-10-11 14:39:11 +0530

    Cahnged App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import Header from './pages/Header'
 
 export default function App() {
   return (
-    <div>App</div>
+    <div>
+      <Header/>
+      <h1>New Change</h1>
+
+    </div>
   )
 }
