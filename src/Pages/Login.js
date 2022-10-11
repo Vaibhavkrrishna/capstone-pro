96d91739b46e847de526ffac6b8c7aa75fb7d565
irenecyriac <irenecyriac20@gmail.com>
2022-10-11 16:23:24 +0530

    Update Login.js

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,13 +1,7 @@
 import React from 'react'
-import {useState} from 'react'
 
 export default function Login() {
-  let [show,setShow] = useState(true);
   return (
-    <div>
-      <button onClick={()=>{
-        setShow(!show)
-      }}>Login</button>
-    </div>
+    <div>Login</div>
   )
 }
